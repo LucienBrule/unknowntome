eb4b6880a23261f0cbeb4cf86ab05c5126daa606
Max Wang <wangm13@rpi.edu>
2015-11-14 15:14:30 -0500

    new file:   quirk.js

---
+++ quirk.js
@@ -0,0 +1,14 @@
+class quirk {
+	
+	constructor(title, geoLoc, sentiment, description, numReviews, specialAccess) {
+		this.title = title;
+		this.geoLoc = geoLoc;
+		this.sentiment = sentiment;
+		this.description = description;
+		this.numReviews = numReviews;
+		this.specialAccess = specialAccess;
+	}
+
+	
+
+} 
