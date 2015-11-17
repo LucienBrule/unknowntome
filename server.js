41bee1948bb2dbb8e70e2e0d915e5217cc3a77b3
Lucien Brule <brulel@rpi.edu>
2015-11-16 19:05:54 -0500

    Made website use api and load all demo points

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-var app = require('./server');
+var app = require(__dirname + '/server'); //Hacky solutution to my problems
 var express = require('express');
-var app = express();
+var app = express();                      //Notice how the hack solution doesn't actually matter for in ap operability
 var fs = require('fs');
 var Datastore = require('nedb')
 var sentiment = require('sentiment');
 
 
-var quirk = require('./js/quirk');
-var parking = require('./js/parking')
-var bathroom = require('./js/bathroom')
-var waterfountain = require('./js/waterfountain')
-var meetinglocation = require('./js/meetinglocation')
+var quirk = require(__dirname + '/js/quirk');
+var parking = require(__dirname + '/js/parking')
+var bathroom = require(__dirname + '/js/bathroom')
+var waterfountain = require(__dirname + '/js/waterfountain')
+var meetinglocation = require(__dirname + '/js/meetinglocation')
 
-var defaultquirks = require('./defaultjson/defaultquirks');
-var defaultparkingspots = require('./defaultjson/defaultparkingspots');
-var defaultbathrooms = require('./defaultjson/defaultbathrooms');
-var defaultwaterfountains = require('./defaultjson/defaultwaterfountains');
-var defaultmeetinglocations = require('./defaultjson/defaultmeetinglocations');
+var defaultquirks = require(__dirname + '/defaultjson/defaultquirks');
+var defaultparkingspots = require(__dirname + '/defaultjson/defaultparkingspots');
+var defaultbathrooms = require(__dirname + '/defaultjson/defaultbathrooms');
+var defaultwaterfountains = require(__dirname + '/defaultjson/defaultwaterfountains');
+var defaultmeetinglocations = require(__dirname + '/defaultjson/defaultmeetinglocations');
 
 var quirks = new Datastore({ filename: __dirname + '/nedbs/quirks.json', autoload: true });
 var parkingspots = new Datastore({ filename: __dirname + '/nedbs/parkingspots.json', autoload: true });
@@ -25,17 +25,26 @@ var waterfountains = new Datastore({ filename: __dirname + '/nedbs/waterfountain
 var meetinglocations = new Datastore({ filename: __dirname + '/nedbs/meetinglocations.json', autoload: true });
 
 var webpage = "<";
-
-
+var options = {
+    root: __dirname + '/public/',
+    dotfiles: 'deny',
+    fallthrough: true,
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+app.use("/",express.static(__dirname + "/public",options))
 //Serves the webpage
-app.get('/', function (req, res) {
-     res.sendfile('./public/index.html');
-});
+// app.get('/', function (req, res) {
+//      res.sendFile('./public/index.html');
+// });
 
-app.get('/defaultquirks.json', function(req, res)
-{
-  res.sendfile('./defaultquirks.json');
-});
+// app.get('/defaultquirks.json', function(req, res)
+// {
+//   res.sendFile('./defaultjson/defaultquirks.json');
+// });
+
 // app.get('/', function (req, res) {
 //   var hostname = req.headers.host.split(":")[0];
 //   if(hostname.slice(0,6) == "quirks")
@@ -46,10 +55,10 @@ app.get('/defaultquirks.json', function(req, res)
 // });
 
 //Host the static webpage
-app.use(express.static('./public'));
+// app.use(express.static('./public'));
 
 //route the quirks
-app.put('/quirk', function (req, res) {
+app.put('/api/quirk', function (req, res) {
   res.send('Got a PUT request at /user');
   console.log("User %s attempted to PUT at /user",req.connection.remoteAddress);
   var myquirk = new quirk(req.query.title,req.query.geoloc,
@@ -59,12 +68,12 @@ app.put('/quirk', function (req, res) {
     console.log("add successfull");
   });
 });
-app.get('/quirks/all', function (req, res) {
+app.get('/api/quirks/all', function (req, res) {
   quirks.find({},function (err, docs) {
     res.send(JSON.stringify(docs));
   });
 });
-app.get('/quirks/byid',function (req, res) {
+app.get('/api/quirks/byid',function (req, res) {
   quirks.find({_id:String(req.query._id)},function (err, docs) {
     res.send(JSON.stringify(docs));
   });
@@ -79,21 +88,21 @@ app.get('quirks/bysentiment',function (req, res) {
     res.send(JSON.stringify(docs));
   });
 });
-app.put('/quirk/comment', function (req, res) {
+app.put('/api/quirk/comment', function (req, res) {
  res.send("OK")
  quirks.find({title:req.query.title},function(err,docs){
   var mysent = String(parseInt(docs.sentiment) + parseInt(sentiment(req.query.comment))/2);
   quirks.update({title: req.query.title},{$set:{sentiment:mysent}});
   });
 });
-app.delete('/quirk/id', function (req, res) {
+app.delete('/api/quirk/id', function (req, res) {
   db.remove({_id:res.query._id},function (err, numRemoved) {
     console.log("removed some quirks");
   });
 });
 
 //route parking spaces
-app.put('/parkingspot', function (req, res) {
+app.put('/api/parkingspot', function (req, res) {
   res.send('Got a PUT request at /user');
   console.log("User %s attempted to PUT at /user",req.connection.remoteAddress);
   var myparkingspot = new parkingspot(req.query.title,req.query.start,req.query.stop);
@@ -101,12 +110,12 @@ app.put('/parkingspot', function (req, res) {
     console.log("add successfull");
   });
 });
-app.get('/parkingspots/all', function (req, res) {
+app.get('/api/parkingspots/all', function (req, res) {
   parkingspots.find({},function (err, docs) {
     res.send(JSON.stringify(docs));
   });
 });
-app.get('/parkingspots/byid',function (req, res) {
+app.get('/api/parkingspots/byid',function (req, res) {
   parkingspots.find({_id:req.query._id},function (err, docs) {
     res.send(JSON.stringify(docs));
   });
@@ -116,7 +125,7 @@ app.get('qurirks/bytitle',function (req, res) {
     res.send(JSON.stringify(docs));
   });
 });
-app.delete('/parkingspot/id', function (req, res) {
+app.delete('/api/parkingspot/id', function (req, res) {
   db.remove({_id:res.query._id},function (req, res) {
       console.log("removed some parkingspots");
 
@@ -124,7 +133,7 @@ app.delete('/parkingspot/id', function (req, res) {
 });
 
 //route bathrooms
-app.put('/bathroom', function (req, res) {
+app.put('/api/bathroom', function (req, res) {
   res.send('Got a PUT request at /user');
   console.log("User %s attempted to PUT at /user",req.connection.remoteAddress);
   var mybathroom = new bathroom(req.query.title,req.query.geoloc,
@@ -134,11 +143,11 @@ app.put('/bathroom', function (req, res) {
     console.log("add successfull");
   });
 });
-app.get('/bathrooms/all', function (req, res) {
+app.get('/api/bathrooms/all', function (req, res) {
   bathrooms.find({},function (err, docs) {
     res.send(JSON.stringify(docs));
   });});
-app.get('/bathrooms/byid',function (req, res) {
+app.get('/api/bathrooms/byid',function (req, res) {
   bathrooms.find({_id:req.query._id},function (err, docs) {
     res.send(JSON.stringify(docs));
   });
@@ -152,14 +161,14 @@ app.get('bathrooms/bysentiment',function (req, res) {
   bathrooms.find({sentiment: req.query.sentiment},function (err, docs) {
     res.send(JSON.stringify(docs));
   });});
-app.delete('/bathroom/id', function (req, res) {
+app.delete('/api/bathroom/id', function (req, res) {
   db.remove({_id:res.query._id},function (req, res) {
     console.log("removed some bathroom/s");
   });
 });
 
 //route meeting locations
-app.put('/meetinglocation', function (req, res) {
+app.put('/api/meetinglocation', function (req, res) {
   res.send('Got a PUT request at /user');
   console.log("User %s attempted to PUT at /user",req.connection.remoteAddress);
   var mymeetinglocation = new meetinglocation(req.query.title,req.query.geoloc,
@@ -169,15 +178,15 @@ app.put('/meetinglocation', function (req, res) {
     console.log("add successfull");
   });
 });
-app.get('/meetinglocations/all', function (req, res) {
+app.get('/api/meetinglocations/all', function (req, res) {
   meetinglocations.find({},function (err, docs) {
     res.send(JSON.stringify(docs));
   });  });
-app.get('/meetinglocations/byid',function (req, res) {
+app.get('/api/meetinglocations/byid',function (req, res) {
   meetinglocations.find({_id:String(req.query._id)},function (err, docs) {
     res.send(JSON.stringify(docs));
   });  });
-app.get('/meetinglocations/bytitle',function (req, res) {
+app.get('/api/meetinglocations/bytitle',function (req, res) {
   meetinglocations.find({title: req.query.title},function (err, docs) {
     res.send(JSON.stringify(docs));
   });
@@ -187,10 +196,10 @@ app.get('meetinglocations/bysentiment',function (req, res) {
     res.send(JSON.stringify(docs));
   });
 });
-app.post('/meetinglocation', function (req, res) {
+app.post('/api/meetinglocation', function (req, res) {
     meetinglocations.update({title: req.query.title})
 });
-app.delete('/meetinglocation/id', function (req, res) {
+app.delete('/api/meetinglocation/id', function (req, res) {
     db.remove({_id:res.query._id},function (req, res) {
       console.log("removed some meetinglocations");
     });
@@ -198,7 +207,7 @@ app.delete('/meetinglocation/id', function (req, res) {
 
 //route water fountains
 app.route("/waterfountain");
-app.put('/waterfountain', function (req, res) {
+app.put('/api/waterfountain', function (req, res) {
     res.send('Got a PUT request at /user');
     console.log("User %s attempted to PUT at /user",req.connection.remoteAddress);
     var mywaterfountain = new waterfountain(req.query.title,req.query.geoloc,
@@ -208,12 +217,12 @@ app.put('/waterfountain', function (req, res) {
       console.log("add successfull");
     });
 });
-app.get('/waterfountains/all', function (req, res) {
+app.get('/api/waterfountains/all', function (req, res) {
   waterfountains.find({},function (err, docs) {
     res.send(JSON.stringify(docs));
   });
 });
-app.get('/waterfountains/byid',function (req, res) {
+app.get('/api/waterfountains/byid',function (req, res) {
   waterfountains.find({_id:String(req.query._id)},function (err, docs) {
     res.send(JSON.stringify(docs));
   });
@@ -228,13 +237,27 @@ app.get('waterfountains/bysentiment',function (req, res) {
     res.send(JSON.stringify(docs));
   });
 });
-app.post('/waterfountain', function (req, res) {
+app.post('/api/waterfountain', function (req, res) {
     waterfountains.update({title: req.query.title})
   });
-app.delete('/waterfountain/id', function (req, res) {
+app.delete('/api/waterfountain/id', function (req, res) {
     db.remove({_id:res.query._id},function (req, res) {
       console.console.log("removed some waterfountains");
     });
+});
+
+
+app.get("/:name",function (req,res,next){
+  fileName = req.params.name;
+  res.sendFile(fileName,options,function(err){
+    if(err){
+      console.log(err);
+      res.status(err.status).end();
+    }
+    else{
+      console.log('Sent: ', filename);
+    }
+  });
 });
 
 //start the server , the entry point is here.
@@ -267,6 +290,7 @@ var server = app.listen(process.env.PORT || 3000, function () {
       process.exit(0);
     }
   });
+
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
