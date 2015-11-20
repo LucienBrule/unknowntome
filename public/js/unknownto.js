3d018c2aa482be8897b56727d2e8421ba84ca455
Jonathan Caicedo <jbirdcaicedo@yahoo.com>
2015-11-20 10:38:16 -0500

    Fixed the infamous map scroll bug.

--- public/js/unknownto.js
+++ public/js/unknownto.js
@@ -12,7 +12,7 @@ $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top }, 1500, 'easeInOutExpo');
+            scrollTop: $($anchor.attr('href')).offset().top}, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
 });
